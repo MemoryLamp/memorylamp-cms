1fcf363b07aaade609c72cc727e313884aeef641
Pyl Batu <pyl@rightvalley.com>
2021-01-30 03:47:29 +0800

    Style button

--- src/components/buttons/SimpleButton.jsx
+++ src/components/buttons/SimpleButton.jsx
@@ -1,8 +1,13 @@
 function SimpleButton(props) {
   return (
-    <button className="bg-blue-700 hover:bg-blue-500 transition duration-50 px-5 py-2 rounded-lg shadow text-white block">
-      {props.text}
-    </button>
+    <>
+      <button
+        type="submit"
+        className="group relative w-auto flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      >
+        {props.text}
+      </button>
+    </>
   );
 }
 
