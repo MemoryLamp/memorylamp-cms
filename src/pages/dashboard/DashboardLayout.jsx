6a8f5a499ed6ff394d34c77c9b16c484fba50e35
Pyl Batu <pyl@rightvalley.com>
2021-01-30 04:02:00 +0800

    Polish UI

--- src/pages/dashboard/DashboardLayout.jsx
+++ src/pages/dashboard/DashboardLayout.jsx
@@ -16,26 +16,38 @@ export function SelectedPage(props) {
   }
 }
 
+function NavLink(props) {
+  return (
+    <>
+      <li>
+        <Link className="font-bold" to={props.link}>
+          {props.text}
+        </Link>
+      </li>
+    </>
+  );
+}
+
 export default function DashboardLayout(props) {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/translations">Translations</Link>
-          </li>
-          <li>
-            <Link to="/verses">Verses</Link>
-          </li>
-          <li>
-            <Link to="/collections">Collections</Link>
-          </li>
-          <li>
-            <Link to="/">Log out</Link>
-          </li>
+    <div className="flex flex-row">
+      <nav className="h-screen px-8  bg-gray-100 shadow border">
+        <div className="pt-8">
+          <p className="mx-auto w-auto text-center font-extrabold text-indigo-600">
+            MemoryLamp
+          </p>
+        </div>
+        <ul className="pt-12 space-y-2 text-gray-800">
+          <NavLink link="/translations" text="Translations" />
+          <NavLink link="/verses" text="Verses" />
+          <NavLink link="/collections" text="Collections" />
+          <hr />
+          <NavLink link="/" text="Logout" />
         </ul>
       </nav>
-      <SelectedPage page={props.page} />
+      <div className="container px-8 pt-8">
+        <SelectedPage page={props.page} />
+      </div>
     </div>
   );
 }
--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -13,7 +13,9 @@ function LoginPage() {
       <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
-            <p className="mx-auto w-auto text-center font-extrabold">LOGO</p>
+            <p className="mx-auto w-auto text-center font-extrabold text-indigo-600">
+              LOGO
+            </p>
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
               Memory Lamp
             </h2>
